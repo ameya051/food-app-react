e60df49a9915d07aea67fedf940893d8e4951721
Ameya Shrivastava <75047964+ameya051@users.noreply.github.com>
2023-06-16 13:44:06 +0530

    deployment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
